58a5541e9047c78c3c1b459b90bb90164fc4b648
kingsRepos <kingsa2128@gmail.com>
2024-04-14 23:34:57 +0100

    new3

---
+++ src/config/env.ts
@@ -0,0 +1,9 @@
+export class Env {
+  public static readonly browser = "firefox";
+  public static readonly APP_URL = "http://localhost/login/index.php/";
+  public static readonly COURSES_URL =
+    "http://localhost/admin/search.php#linkcourses";
+  public static readonly ADMIN_USER1 = "admin";
+  public static readonly ADMIN_USER1_PASSWORD = "Admin-123";
+
+}
