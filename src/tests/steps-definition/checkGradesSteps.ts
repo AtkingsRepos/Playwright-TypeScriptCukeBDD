901e28e2a69a3028808be246f8d411894fe9036e
kingsRepos <kingsa2128@gmail.com>
2024-04-22 14:38:14 +0100

    report jpg1

--- src/tests/steps-definition/checkGradesSteps.ts
+++ src/tests/steps-definition/checkGradesSteps.ts
@@ -8,40 +8,39 @@ import {
 } from "@cucumber/cucumber";
 import { Page, expect } from "@playwright/test";
 import { getPage } from "../../hooks/hooks";
-import GradePage from "../../tests/pages/gradesPage"
+import GradePage from "../../tests/pages/gradesPage";
 import SiteNavigation from "../pages/siteNavigation";
 import SiteAdministrationPage from "../pages/siteAdministrationPage";
 
-
 Given("I am on the grades tab", async function () {
-  this.log(">>>>>>>:Clicking Grades Link Page>>>>>>")
-    const siteNavigation = new SiteNavigation(getPage());
+  this.log(">>>>>>>:Clicking Grades Link Page>>>>>>");
+  const siteNavigation = new SiteNavigation(getPage());
   (await siteNavigation.navigateToSiteAdministrationPage()).navigateToGrades();
   this.log(">>>>>>>:Clicked on Grades Link Page and navigated to Grades>>>>>>");
 });
-When("I go to grades setting", async function (){
+When("I go to grades setting", async function () {
   const gradePage = new GradePage(getPage(), this.log);
   this.log(">>>>>>>:Clicking on Grades Tab>>>>>>");
   await gradePage.clickGradesTab();
-   this.log(">>>>>>>:Clicked on Grades Tab and navigating letters link>>>>>>");
+  this.log(">>>>>>>:Clicked on Grades Tab and navigating letters link>>>>>>");
   await gradePage.clickLettersLink();
   this.log(">>>>>>>:Clicked on leter link>>>>>>");
-   this.log(">>>>>>>:Clicking  on Edit button>>>>>>");
+  this.log(">>>>>>>:Clicking  on Edit button>>>>>>");
   await gradePage.clickEditButton();
-   this.log(">>>>>>>:Clicked on Edit button and navigating to save changes>>>>>>");
-    
+  this.log(
+    ">>>>>>>:Clicked on Edit button and navigating to save changes>>>>>>"
+  );
 });
 Then("I should edit grades,save the changes", async function () {
-     const gradePage = new GradePage(getPage(),this.log);
+  const gradePage = new GradePage(getPage(), this.log);
   await gradePage.clickSaveChangesButton();
-   this.log(">>>>>>>:Clicked on save button>>>>>>");
+  this.log(">>>>>>>:Clicked on save button>>>>>>");
   console.log(">>>>>>>>>", await gradePage.isAlertMessageDisplayed());
   const expectedText =
-    "The default grade letters are currently overridden. × Dismiss this notification";
-  console.log(">>>>>>>>>>expected Text:",expectedText);
-  const Actual  = await gradePage.getAlertMessageText();
+    "The default grade letters are currently overridden. × Dismiss this notificationxxxxx";
+  console.log(">>>>>>>>>>expected Text:", expectedText);
+  const Actual = await gradePage.getAlertMessageText();
   console.log(">>>>>>>>>>Actual Text  :  ", Actual);
- //expect(expectedText).toEqual(Actual);
-
+  //expect(expectedText).toEqual(Actual);
 });
 //"The default grade letters are currently overridden. × Dismiss this notification";
